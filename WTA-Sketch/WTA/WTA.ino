0fe77339d7bceb2304f4e9e628678da654d1c1bd
Jared <J@COde.com>
2024-03-12 13:57:55 -0600

    Changed code to be more inline with Conceptual drawing. Note physical ware were is expected to be in the following form starting at pin 2 toggleSwitch, Higher, Lower With Pin A1 in place after pin 13

--- WTA-Sketch/WTA/WTA.ino
+++ WTA-Sketch/WTA/WTA.ino
@@ -1,50 +1,57 @@
 
 //Sends Serial output when a state changes
 // Ie every button press we get output for pressed and released
-// Format is T[0],E[0],D[0],H[0],L[0]
+// Format is X[0] where X is key and 0 is value
 // Has Digital pin debouncing and threshold value for pot changes
-
-
-int button1 = 2;
-int button2 = 3;
-int button3 = 4;
-int button4 = 5;
+// Button are expected to be ordered toggle, higher, lower in the buttonValues array
+//Pin 2-13 A1,A2,A3,A4 Buttin Inputs
 int potIn = A0;
-
-
-int values[] = {0,0,0,0,0};
-int previousValues[] = {0,0,0,0,0};
+bool enabled[] = {false, false,false,false,false};
+int sensorValue = 0;
+int previousSensor = 0;
+int buttonValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int previousValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int bounceDelay = 5;
-int ignorePin[] = {0,0,0,0,0};
+int ignorePin[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int threshold = 5;
 
 void setup() {
-  pinMode(button1,INPUT);
-  pinMode(button2,INPUT);
-  pinMode(button3,INPUT);
-  pinMode(button4,INPUT);
+  for(int i=2;i<14;i++)
+  {
+    pinMode(i,INPUT);
+  }
+  
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int rawSensor = analogRead(potIn);
-  values[0] = map(rawSensor,0,1023,0,254);
-  values[1] = digitalRead(button1);
-  values[2] = digitalRead(button2);
-  values[3] = digitalRead(button3);
-  values[4] = digitalRead(button4);
+  sensorValue = map(rawSensor,0,1023,0,254);
+  for(int i=2;i<14;i++)
+  {
+    buttonValues[i-2] = digitalRead(i);
+  }
+  buttonValues[11] = digitalRead(A1);
+  buttonValues[12] = digitalRead(A2);
+  buttonValues[13] = digitalRead(A3);
+  buttonValues[14] = digitalRead(A4);
 
   bool stateChanged = false;
-  if(abs(values[0] - previousValues[0])>threshold){
-    stateChanged = true;
-    previousValues[0] = values[0];
+  if(abs(sensorValue - previousSensor)>threshold){
+    processsChange(-1);
+    previousSensor = sensorValue;
   }
-  for(int i=1;i<5;i++){
+  for(int i=0;i<15;i++){
     if(ignorePin[i]<=0){
-      if(previousValues[i] != values[i]){
-        stateChanged = true;
-        previousValues[i] = values[i];
+      if(previousValues[i] != buttonValues[i]){
+        processsChange(i);
+        previousValues[i] = buttonValues[i];
         ignorePin[i] = bounceDelay;
     }
     }else{
@@ -52,27 +59,42 @@ void loop() {
     }
     
   }
-
-  if(stateChanged){
-    SendOutput();
-  }
   
 
   delay(10);
 
 }
-//Print output correctly formatted
-void SendOutput()
+
+//Print changed to Serial
+void SendUpdate(char key, int value){
+  Serial.print(key);
+  Serial.print("[");
+  Serial.print(value);
+  Serial.println("]");
+}
+
+//Logic to control what is printed to Serial and how array is layed out
+void processsChange(int index)
 {
-  Serial.print("T[");
-  Serial.print(values[0]);
-  Serial.print("],E[");
-  Serial.print(values[1]);
-  Serial.print("],D[");
-  Serial.print(values[2]);
-  Serial.print("],H[");
-  Serial.print(values[3]);
-  Serial.print("],L[");
-  Serial.print(values[4]);
-  Serial.print("]\n");
+  int remainder = index % 3;
+  if(index == -1)
+  {
+    SendUpdate('T',sensorValue);
+  }else if(remainder == 0){
+    int buttonIndex = index/3;
+    if(enabled[buttonIndex]){
+      SendUpdate('D',buttonIndex);
+      enabled[buttonIndex] = false;
+    }else{
+      SendUpdate('E',buttonIndex);
+      enabled[buttonIndex] = true;
+    }
+  }else if(remainder == 1)
+  {
+    int controlIndex = index/3;
+    SendUpdate('H',controlIndex);
+  }else if (remainder == 2){
+    int controlIndex = index/3;
+    SendUpdate('L',controlIndex);
+  }
 }
